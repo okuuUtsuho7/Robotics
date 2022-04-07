dc6b2fca6b424a1a8862d206280c728f4002078c
RPGMN777 <89574595+RPGMN777@users.noreply.github.com>
2022-04-07 16:35:52 +0800

    Add files via upload

---
+++ array.ino
@@ -0,0 +1,31 @@
+//Array programming because I actually missed last session lol
+
+
+// intigers
+int LEDpin[5] = { 2, 3, 4, 5, 6 };
+long ranPins;
+
+
+//LEDS
+void setup()
+{
+	pinMode(LEDpin[0], OUTPUT); //pin 1
+	pinMode(LEDpin[1], OUTPUT); //pin 2
+	pinMode(LEDpin[2], OUTPUT); //pin 3
+	pinMode(LEDpin[3], OUTPUT); //pin 4
+	pinMode(LEDpin[4], OUTPUT); //pin 5
+	randomSeed(analogRead(0));
+}
+
+
+//RNG go brrrrr
+void loop()
+{
+	ranPins = random(2, 7);
+	
+	digitalWrite(ranPins, HIGH);
+	delay(300);
+
+	digitalWrite(ranPins, LOW);
+	delay(300);
+}
